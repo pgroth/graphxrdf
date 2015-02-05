a92b89513a3a33fc2915f3d62b747cef3ac239b7
lukovnikov <lukovnikov@denis>
2015-02-05 19:54:17 +0100

    qsdf

--- src/main/scala/org/lukovnikov/graphxrdf/RDFLoad.scala
+++ src/main/scala/org/lukovnikov/graphxrdf/RDFLoad.scala
@@ -35,33 +35,33 @@ object RDFLoad {
 	def rwr(graph:Graph[String,String], numiter:Int = 2) = {
 		var outdegrees = graph.outDegrees.map(
 				outdeg => 
-					(outdeg._1, (outdeg._2, Map[Long,Double](), Map[Long, Double](outdeg._1 -> 1.0)))
+					(outdeg._1, (outdeg._2, Map[Long, Double](outdeg._1 -> 1.0)))
 				)
-		var g2 = Graph[(Int,Map[Long, Double],Map[Long, Double]), String](outdegrees, graph.edges)
+		var inbox = Graph[(Int,Map[Long, Double]), String](outdegrees, graph.edges)
 		for (vertex <- outdegrees.collect)
 			Console.println(vertex)
 		//g2 = g2.mapEdges(edge => null)
 		var iter = numiter
 		while (iter > 0) {
-			val agg = g2.aggregateMessages[(Int,Map[Long, Double],Map[Long,Double])](
+			val agg = inbox.aggregateMessages[(Int,Map[Long,Double])](
 				triplet => {
 					val distId = triplet.dstId
 					val sourcedeg = triplet.srcAttr._1
 					var ret = Map[Long, Double]()//Map(distId -> 1.0/sourcedeg)
 					if (triplet.dstAttr != null)
-						for (entry <- triplet.dstAttr._3) {
+						for (entry <- triplet.dstAttr._2) {
 							ret += (entry._1 -> entry._2/sourcedeg)
 						}
-					triplet.sendToSrc((sourcedeg, triplet.srcAttr._2, ret))
+					triplet.sendToSrc((sourcedeg, ret))
 				},
 				(a,b) => {
 					// merge inboxes
 					val degree = a._1
 					var inbox = scala.collection.mutable.Map[Long, Double]()
-					for (axe <- a._3) {
+					for (axe <- a._2) {
 						inbox(axe._1) = axe._2
 					}
-					for (bxe <- b._3) {
+					for (bxe <- b._2) {
 						if (inbox.contains(bxe._1)) {
 							inbox(bxe._1) += bxe._2
 						} else {
@@ -69,22 +69,22 @@ object RDFLoad {
 						}
 					}
 					// merge inbox into state
-					var state = scala.collection.mutable.Map() ++ a._2
+					/* var state = scala.collection.mutable.Map() ++ a._2
 					for (x <- inbox) {
 						if (state.contains(x._1)) {
 							state(x._1) += x._2
 						} else {
 							state(x._1) = x._2
 						}
-					}
-					(degree, Map() ++ state, Map() ++ inbox)
+					} // */
+					(degree, Map() ++ inbox) 
 				}
 			)
-			g2 = Graph(agg, g2.edges)
+			inbox = Graph(agg, inbox.edges)
 			iter -= 1
 		}
 		val origvertices = graph.vertices.collect
-		val aggrvertices = g2.vertices.collect
+		val aggrvertices = inbox.vertices.collect
 		for (vertex <- aggrvertices) {
 			Console.println(vertex)
 			//Console.println(vertex._1.toString + ": " + vertex._2.toString)
